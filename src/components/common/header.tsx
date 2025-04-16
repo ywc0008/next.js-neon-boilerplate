f51d521c034b51279e494562ad5185e7fcd5354f
leewc <ywc0008@naver.com>
2025-04-16 17:37:20 +0900

    feat: improve responsive container and header proportions

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -4,37 +4,35 @@ import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import SignOutButton from "./sign-out-button";
 import { GithubIcon } from "./social-icons";
+import { AuroraText } from "../magicui/aurora-text";
 
 export default async function Header() {
-  const session = await auth();
+	const session = await auth();
 
-  return (
-    <header className="py-4 px-6 md:px-12 flex items-center justify-between border-b">
-      {/* 로고 영역 */}
-      <div className="z-10">
-        <Link href="/" className="flex items-center gap-2">
-          <h1 className="text-xl md:text-3xl font-bold text-green-500">
-            Next.js+Neon
-          </h1>
-        </Link>
-      </div>
+	return (
+		<header className="sticky top-0 z-50 w-full flex items-center justify-center py-4 px-6 md:px-12 border-b bg-white">
+			<div className="container flex items-center justify-between">
+				{/* 로고 영역 */}
+				<div className=" z-10">
+					<Link href="/" className="flex items-center gap-2">
+						<AuroraText className="text-xl md:text-3xl font-bold text-green-500">Next.js+Neon</AuroraText>
+					</Link>
+				</div>
 
-      {/* 사용자 영역 */}
-      <div className="flex items-center gap-2 z-10">
-        <Link
-          target="_blank"
-          href="https://github.com/ywc0008/next.js-neon-boilerplate"
-        >
-          <GithubIcon className="size-6" />
-        </Link>
-        {session?.user ? (
-          <SignOutButton session={session} />
-        ) : (
-          <Button asChild variant="outline">
-            <Link href="/sign-in">Sign In</Link>
-          </Button>
-        )}
-      </div>
-    </header>
-  );
+				{/* 사용자 영역 */}
+				<div className="flex items-center gap-2 z-10">
+					<Link target="_blank" href="https://github.com/ywc0008/next.js-neon-boilerplate">
+						<GithubIcon className="size-6" />
+					</Link>
+					{session?.user ? (
+						<SignOutButton session={session} />
+					) : (
+						<Button asChild variant="outline">
+							<Link href="/sign-in">Sign In</Link>
+						</Button>
+					)}
+				</div>
+			</div>
+		</header>
+	);
 }
