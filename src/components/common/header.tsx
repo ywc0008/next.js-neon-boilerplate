a7c8f33446333ba64a279b9cee5500fe78158456
leewc <ywc0008@naver.com>
2025-04-18 17:33:53 +0900

    feat: add animated username greeting with word rotation effect

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import { auth } from "@/auth";
 
 import { Button } from "@/components/ui/button";
-import SignOutButton from "./sign-out-button";
+
 import { GithubIcon } from "./social-icons";
+import SignOutButton from "./sign-out-button";
 import { AuroraText } from "../magicui/aurora-text";
+import { WordRotate } from "../magicui/word-rotate";
 
 export default async function Header() {
 	const session = await auth();
@@ -12,15 +14,18 @@ export default async function Header() {
 	return (
 		<header className="sticky top-0 z-50 w-full flex items-center justify-center py-4 px-6 md:px-12 border-b bg-white">
 			<div className="container flex items-center justify-between">
-				{/* 로고 영역 */}
+				{/* Logo */}
 				<div className=" z-10">
 					<Link href="/" className="flex items-center gap-2">
 						<AuroraText className="text-xl md:text-3xl font-bold text-green-500">Next.js+Neon</AuroraText>
 					</Link>
 				</div>
 
-				{/* 사용자 영역 */}
+				{/* User */}
 				<div className="flex items-center gap-2 z-10">
+					{session?.user && (
+						<WordRotate className="text-xl font-bold mr-2" words={["Hello", `${session.user.name}!`]} duration={2000} />
+					)}
 					<Link target="_blank" href="https://github.com/ywc0008/next.js-neon-boilerplate">
 						<GithubIcon className="size-6" />
 					</Link>
