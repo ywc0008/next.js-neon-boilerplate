b9d5f54a3ddcbdf3b84cd935246520998eecc989
leewc <ywc0008@naver.com>
2025-04-18 16:35:14 +0900

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				{/* Favicon */}
+				<link rel="icon" href="/favicon/favicon.ico" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+			</head>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
 		</html>
 	);
